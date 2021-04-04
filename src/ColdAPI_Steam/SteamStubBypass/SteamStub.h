334956ae0508dae50abc4da9413ba96f354a9225
kettenbruch <kettenbruch@users.noreply.github.com>
2021-04-04 22:38:19 +0200

    ready for inital release

--- src/ColdAPI_Steam/SteamStubBypass/SteamStub.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Windows.h>
-#include "../Bridge.h"
-namespace SteamStubBypass
-{
-	bool PatchSteamStub(ULONG_PTR BindStart, ULONG_PTR BindEnd, ULONG_PTR entryexe);
-	void Patch();
-}
